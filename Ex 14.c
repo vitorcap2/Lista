87f51cce3f0829b3f311451c757079402f8f60ee
vitorcap2 <43814616+vitorcap2@users.noreply.github.com>
2018-12-02 20:50:32 -0200

    Ex 14

---
+++ Ex 14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	printf ("					XXXXX");
+	printf ("\n					X   X");
+	printf ("\n					X   X");
+	printf ("\n					X   X");
+	printf ("\n					XXXXX");
+	
+	
+	
+printf ("\n\n\n");
+system ("pause");
+}
