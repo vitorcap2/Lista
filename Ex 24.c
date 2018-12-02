7dd5f987c50207422da2434e5605cad5915337b2
vitorcap2 <43814616+vitorcap2@users.noreply.github.com>
2018-12-02 20:51:44 -0200

    Ex 24

---
+++ Ex 24.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main()
+{
+	int a,b,c;
+	printf("Informe o valor de A:");
+	scanf("%d", &a);
+	printf("Informe o valor de B:");
+	scanf("%d", &b);
+	c = a;
+	a = b;
+	b = c;
+	printf("\nValor de A trocado: %d", a);
+	printf("\nValor de B trocado: %d", b);
+	
+	
+	
+	
+	
+printf ("\n\n\n");
+system("pause");
+}
