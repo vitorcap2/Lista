2741176b84957b30d55ef0753cb2227a653a06ef
vitorcap2 <43814616+vitorcap2@users.noreply.github.com>
2018-12-02 20:50:05 -0200

    Ex 13

---
+++ Ex 13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+int i,num, soma=0;
+
+printf ("Informe um numero inteiro: ");
+scanf("%d", &num);
+for(i=0;i<num;i++)
+printf(" %d ", i*2+1);
+
+	
+	
+	
+printf ("\n");
+system ("pause");
+}
