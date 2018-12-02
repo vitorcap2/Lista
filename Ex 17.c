00ed59c618e826062626bcf72af27a9c03df1e4f
vitorcap2 <43814616+vitorcap2@users.noreply.github.com>
2018-12-02 20:51:09 -0200

    Ex 17

---
+++ Ex 17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	  
+    	printf("         X\n");
+        printf("        XXX\n");
+        printf("       XXXXX\n");
+     	printf("      XXXXXXX\n");
+        printf("     XXXXXXXXX\n");
+        printf("    XXXXXXXXXXX\n");
+        printf("   XXXXXXXXXXXXX\n");
+        printf("  XXXXXXXXXXXXXXX\n");
+        printf("        XX\n");
+        printf("        XX\n");
+        printf("       XXXX\n");
+	
+	
+printf ("\n\n\n");
+system ("pause");
+}
