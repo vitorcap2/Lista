c0742b8f5eabe03d7336ce52a18eea6557b67bc1
vitorcap2 <43814616+vitorcap2@users.noreply.github.com>
2018-12-02 20:51:32 -0200

    Ex 22

---
+++ Ex 22.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int matrizA[3][3], matrizB[3][3], i, j;
+
+	for (i=0; i<3; i++)
+	{
+		for (j=0; j<3; j++)
+		{
+			printf ("Insira o elemento [%d][%d] das Matriz A\n", i, j);
+			scanf ("%d", &matrizA[i][j]);
+		}
+	}
+
+	for (i=0; i<3; i++)
+	{
+          for (j=0; j<3; j++) 
+		  {
+            printf("%d\t", matrizA[i][j]);
+
+		  }
+		  printf("\n");
+
+	}
+	system("pause");
+
+	for (i=0; i<3; i++)
+	{
+
+			for (j=0; j<3; j++)
+			{
+				matrizB[i][j]=matrizA[j][i];
+			}
+
+	}
+
+	for (i=0; i<3; i++)
+	{
+          for (j=0; j<3; j++) 
+		  {
+            printf("%d\t", matrizB[i][j]);
+
+		  }
+		  printf("\n");
+	}
+
+
+	system("pause");
+	return (0);
+}
